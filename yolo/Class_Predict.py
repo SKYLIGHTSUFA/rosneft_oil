e96ace3a8e9061defd489edc9346d5801ca9b2c5
SKYLIGHTSUFA <sdsv100@gmail.com>
2024-03-21 21:20:00 +0300

    add cv2 window to ctklabel

--- yolo/Class_Predict.py
+++ yolo/Class_Predict.py
@@ -2,6 +2,8 @@ import customtkinter as ctk
 import os
 from ultralytics import YOLO
 import cv2
+from PIL import Image, ImageTk
+
 
 class Predict(ctk.CTkFrame):
     def __init__(self, master):
@@ -16,6 +18,7 @@ class Predict(ctk.CTkFrame):
         self.button_start_predict = ctk.CTkButton(self, text="Start predict", command=self.__start_predict)
         self.button_start_predict.grid(row=2, column=0, pady=10, sticky="ew")
 
+
     def __open_file_dialog(self):
         root = ctk.CTk()
         root.withdraw()
@@ -23,29 +26,25 @@ class Predict(ctk.CTkFrame):
         if file_path != '':
             root.destroy()
             self.storage_name = file_path
+            self.iterator = iter(os.listdir(self.storage_name))
             return file_path
     def __start_predict(self):
         model = YOLO("models/best3_all_data_v8s.pt")
-        for filename in os.listdir(self.storage_name):
+        filename = next(self.iterator)
+        if filename.endswith("png"):
             print(filename)
-            if filename.endswith("png"):
-            #continue
-                results = model.predict(source=os.path.join(self.storage_name, filename), verbose=False)
-                conf = results[0].probs.data.tolist()
-                idx = conf.index(max(conf))
-
-                #cv2.imshow("0", cv2.imread(os.path.join(self.storage_name, filename)))
-                print(f'idx = {idx}, filename = {filename}')
-                image = cv2.imread(os.path.join(self.storage_name, filename))
-                image = cv2.putText(image, ["BAD", "GOOD"][bool(idx!=0)],
-                            (100,100),
-                            cv2.FONT_HERSHEY_SIMPLEX,
-                            fontScale=1,
-                            color=(0, 255, 0),
-                            thickness=2,)
-                # if int(idx) == 1:
-                cv2.imshow("0", image)
-
-                if cv2.waitKey(0) == 27:
-                    break
-        cv2.destroyAllWindows()
+            results = model.predict(source=os.path.join(self.storage_name, filename), verbose=False)
+            conf = results[0].probs.data.tolist()
+            idx = conf.index(max(conf))
+            print(f'idx = {idx}, filename = {filename}')
+            image = cv2.imread(os.path.join(self.storage_name, filename))
+            image = cv2.putText(image, ["BAD", "GOOD"][bool(idx != 0)],
+                                (100, 100),
+                                cv2.FONT_HERSHEY_SIMPLEX,
+                                fontScale=1,
+                                color=(0, 255, 0),
+                                thickness=2, )
+            self.image = Image.fromarray(image)
+            self.image_tk = ctk.CTkImage(self.image, size=(self.image.width, self.image.height))
+            self.label = ctk.CTkLabel(self, image=self.image_tk)
+            self.label.grid(row=3, column=0, pady=10, sticky="ew")
